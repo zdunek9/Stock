8460c5840cbdc3e08cb370e8f1e350d51bed8ed1
Michał Zduńczyk <m.zdunczyk2@gmail.com>
2023-03-20 19:03:35 +0100

    small visual changes

--- src/components/Main/History/History.style.js
+++ src/components/Main/History/History.style.js
@@ -21,11 +21,20 @@ export const SingleItem = styled.li`
   &:nth-child(odd) {
     background-color: #eef1f6;
   }
-  @media (max-width: 900px) {
+
+
+  @media (max-width: 1000px) {
+    grid-template-columns: 15% 20% 28% 17% 20%;
     p {
-      margin: 10px 5px;
-      font-size: 0.6rem;
       overflow: hidden;
+      font-size: 0.8rem;
+      margin: 10px 5px;
+    }
+  }
+  @media (max-width: 700px) {
+    p {
+      font-size: 0.6rem;
+      margin: 4px;
     }
   }
 `;
@@ -41,12 +50,19 @@ export const ListTopBar = styled.li`
     font-weight: 700;
     font-size: 1.3rem;
   }
-  @media (max-width: 900px) {
+
+  @media (max-width: 1000px) {
     grid-template-columns: 15% 20% 28% 17% 20%;
     p {
       margin: 10px 5px;
-      font-size: 0.7rem;
+      font-size: 0.8rem;
       overflow: hidden;
+    }
+  }
+  @media (max-width: 700px) {
+    p {
+      font-size: 0.6rem;
+      margin: 4px;
     }
   }
 `;
